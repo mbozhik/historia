cbc674d57d3f35a35792b9bfc925982a09f74af4
maxim <bozzhik@ya.ru>
2024-06-10 23:01:32 +0300

    init `Container` Global component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image'
+import Container from '#/Global/Container'
 
 export default function Home() {
-  return <h1>hey</h1>
+  return (
+    <Container>
+      <h1 className="w-full text-center py-2 border-2 border-green-400">historia</h1>
+    </Container>
+  )
 }
---
+++ src/components/Global/Container.tsx
@@ -0,0 +1,20 @@
+import {cn} from '@/lib/utils'
+
+interface Props {
+  children: React.ReactNode
+  width?: '1/2' | '2/3'
+  padding?: boolean
+  className?: string
+}
+
+export const containerSize = {
+  default: 'mx-auto flex-auto',
+  '1/2': 'w-[40%] xl:w-[90%]',
+  '2/3': 'w-[80%] xl:w-[90%]',
+}
+
+export default function Container({children, width = '2/3', padding = true, className}: Props) {
+  const containerStyles = `${containerSize.default} ${containerSize[width]} ${padding && 'pt-10'} ${cn(className)}`
+
+  return <main className={cn(containerStyles, className)}>{children}</main>
+}
