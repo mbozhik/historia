0174a6a643030604a55dde40cbbdacffee3ec576
maxim <bozzhik@ya.ru>
2024-07-05 22:20:34 +0300

    init `Text` UI component

---
+++ src/components/UI/Text.tsx
@@ -0,0 +1,17 @@
+import {cn} from '@/lib/utils'
+
+interface Props {
+  type: 'h4' | 'h5'
+  text: string
+  className?: string
+}
+
+const Heading: React.FC<Props> = ({type, text, className}) => {
+  if (type === 'h4') {
+    return <h4 className={cn(`text-2xl tracking-[-0.020em]`, className)} dangerouslySetInnerHTML={{__html: text || ''}} />
+  } else if (type === 'h5') {
+    return <h5 dangerouslySetInnerHTML={{__html: text || ''}} />
+  }
+}
+
+export default Heading
