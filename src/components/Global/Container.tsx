98308b7b2b853dfe28f5a905ebde9537a35fd0b9
maxim <bozzhik@ya.ru>
2024-07-05 22:25:32 +0300

    add `dataSection` prop for `Container` Global component

--- src/components/Global/Container.tsx
+++ src/components/Global/Container.tsx
@@ -4,6 +4,7 @@ interface Props {
   children: React.ReactNode
   width?: '1/2' | '2/3'
   padding?: boolean
+  dataSection?: string
   className?: string
 }
 
@@ -13,8 +14,12 @@ export const containerSize = {
   '2/3': 'w-[80%] xl:w-[90%]',
 }
 
-export default function Container({children, width = '2/3', padding = true, className}: Props) {
+export default function Container({children, width = '2/3', padding = true, className, dataSection}: Props) {
   const containerStyles = `${containerSize.default} ${containerSize[width]} ${padding && 'pt-24'} ${cn(className)}`
 
-  return <main className={cn(containerStyles, className)}>{children}</main>
+  return (
+    <main className={cn(containerStyles, className)} data-section={dataSection}>
+      {children}
+    </main>
+  )
 }
