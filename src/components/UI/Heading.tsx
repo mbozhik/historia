3733b5981f8256f298c54aa68e1c6b3fe5bdae7c
maxim <bozzhik@ya.ru>
2024-07-16 18:44:57 +0300

    add `h3` type for `Heading` UI component

--- src/components/UI/Heading.tsx
+++ src/components/UI/Heading.tsx
@@ -1,16 +1,18 @@
 import {cn} from '@/lib/utils'
 
 interface Props {
-  type: 'h1' | 'h2'
+  type: 'h1' | 'h2' | 'h3'
   text: string
   className?: string
 }
 
 const Heading: React.FC<Props> = ({type, text, className}) => {
   if (type === 'h1') {
-    return <h1 className={cn(`text-6xl xl:text-5xl font-semibold sm:leading-[1.10] tracking-tight`, className)} dangerouslySetInnerHTML={{__html: text || ''}} />
+    return <h1 className={cn('text-6xl xl:text-5xl sm:leading-[1.10]', 'font-semibold tracking-tight', className)} dangerouslySetInnerHTML={{__html: text || ''}} />
   } else if (type === 'h2') {
-    return <h2 className={cn('text-5xl xl:text-4xl sm:text-3xl font-medium tracking-tight', className)} dangerouslySetInnerHTML={{__html: text || ''}} />
+    return <h2 className={cn('text-5xl xl:text-4xl sm:text-3xl', 'font-medium tracking-tight', className)} dangerouslySetInnerHTML={{__html: text || ''}} />
+  } else if (type === 'h3') {
+    return <h3 className={cn('text-4xl xl:text-3xl sm:text-2xl', 'font-medium tracking-tight', className)} dangerouslySetInnerHTML={{__html: text || ''}} />
   }
 }
 
