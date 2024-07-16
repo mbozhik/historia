bea66d1f049e33d00300b66b47357013f180795d
maxim <bozzhik@ya.ru>
2024-07-16 18:43:35 +0300

    init `/profile/author` user page

---
+++ src/app/(user)/profile/author/page.tsx
@@ -0,0 +1,28 @@
+import {getServerSession} from 'next-auth/next'
+import {options} from '@/app/api/auth/[...nextauth]/options'
+
+import Container from '#/Global/Container'
+import Hero from '##/user/profile/Hero'
+import Navigation from '##/user/profile/Navigation'
+import Heading from '#/UI/Heading'
+
+import {Plus} from 'lucide-react'
+
+export default async function ProfilePage() {
+  const session = await getServerSession(options)
+
+  return (
+    <Container dataSection="user-profile" className="space-y-14 xl:space-y-10 sm:space-y-8">
+      <section className="space-y-10 sm:space-y-5">
+        <Hero session={session} />
+
+        <Navigation />
+      </section>
+
+      <section className="mx-auto px-10 py-6 sm:px-6 sm:py-4 w-fit flex flex-col items-center gap-4 sm:gap-2 bg-secondary rounded-[24px] cursor-pointer">
+        <Heading text="Добавить работу" type="h3" className="text-center text-primary" />
+        <Plus className="s-12 xl:s-10 sm:s-8 text-primary" />
+      </section>
+    </Container>
+  )
+}
